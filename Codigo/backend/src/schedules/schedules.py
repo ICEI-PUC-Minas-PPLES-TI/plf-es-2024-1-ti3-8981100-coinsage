4bf566b9ad026d02de64372bfa93dd7aeb10aef7
kspencerl <kimberly.liz@sga.pucminas.br>
2024-04-01 17:38:49 -0300

    Revert "fix/UC01 #42 JVM (#45)"
    This reverts commit 899f647c5a04cd4b51e51419623be118726e4e1c, reversing
    changes made to ebcc954888f3f93c18627977adcca40cbef33a5b.

--- Codigo/backend/src/schedules/schedules.py
+++ Codigo/backend/src/schedules/schedules.py
@@ -1,50 +1,17 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from loguru import logger
-from sqlalchemy.orm import Session
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from sqlalchemy.orm import Session
+# from loguru import logger
 
-from src.config.manager import settings
+# scheduler = AsyncIOScheduler()
 
-# App schedules
-from .update_currencies_info import check_update_currencies_info, update_currencies_info
+# db = None
 
-scheduler = AsyncIOScheduler()
+# def start_schedules(app_db: Session):
+#     global db
+#     db = app_db
+#     scheduler.start()
+#     logger.info("Schedules started")
 
-db = None
-
-
-def start_schedules(app_db: Session):
-    global db
-    db = app_db
-    logger.info("Checking all schedules")
-    check_all_schedules()
-
-    scheduler.start()
-    logger.info("Schedules started")
-
-
-def stop_schedules():
-    scheduler.shutdown()
-    logger.info("Schedules stopped")
-
-
-def check_all_schedules():
-    if db is None:
-        logger.critical("Database session is not available")
-        return
-
-    check_update_currencies_info(db=db, settings=settings.SCHEDULES["update_currencies_info"])
-
-
-# =======  All app schedules =======
-@scheduler.scheduled_job(
-    "cron",
-    hour=settings.SCHEDULES["update_currencies_info"]["hour"],
-    minute=settings.SCHEDULES["update_currencies_info"]["minute"],
-    second=settings.SCHEDULES["update_currencies_info"]["second"],
-    id="update_currencies_info",
-)
-def schedule_update_currencies_info():
-    if db is None:
-        logger.critical("Database session is not available")
-        return
-    update_currencies_info(db=db)
+# def stop_schdeules():
+#     scheduler.shutdown()
+#     logger.info("Schedules stopped")
--- Codigo/backend/src/schedules/update_currencies_info.py
+++ Codigo/backend/src/schedules/update_currencies_info.py
@@ -1,34 +1,42 @@
-from datetime import datetime
+import datetime
 
+# from .schedules import scheduler, db
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from loguru import logger
 from sqlalchemy.orm import Session
 
+from src.config.manager import settings
 from src.models.db.currencies_info_schedule import CurrenciesInfoScheduleModel
 from src.services.currencies_logo_collector import CurrenciesLogoCollector
 
+scheduler = AsyncIOScheduler()
 
-def update_currencies_info(db: Session) -> None:
+db = None
+
+
+def start_schedules(app_db: Session):
+    global db
+    db = app_db
+    scheduler.start()
+    logger.info("Schedules started")
+
+
+def stop_schedules():
+    scheduler.shutdown()
+    logger.info("Schedules stopped")
+
+
+@scheduler.scheduled_job(
+    "cron",
+    hour=settings.SCHEDULES["update_currencies_info"]["hour"],
+    minute=settings.SCHEDULES["update_currencies_info"]["minute"],
+    second=settings.SCHEDULES["update_currencies_info"]["second"],
+    id="update_currencies_info",
+)
+def update_currencies_info():
     logger.info("Updating currencies info")
-    func_session: Session = db
-    CurrenciesLogoCollector(session=func_session).collect_symbols_info()
-
-
-def check_update_currencies_info(db: Session, settings: dict) -> None:
-    next_update_time = (
-        db.query(CurrenciesInfoScheduleModel).order_by(CurrenciesInfoScheduleModel.next_scheduled_time.desc()).first()
-    )
-    now: datetime = datetime.now()
-    settings_time: datetime = now.replace(hour=settings["hour"], minute=settings["minute"], second=settings["second"])
-
-    if next_update_time is not None:
-        if next_update_time.last_update_time.date() == now.date():
-            logger.info("Currencies info already updated today")
-            return
-
-        logger.info("Schedule is late. Updating now")
-        update_currencies_info(db=db)
-        return
-
-    if settings_time < now:
-        logger.info("Schedule never ran. Updating now")
-        update_currencies_info(db=db)
+    if db is not None:
+        func_session: Session = db
+        CurrenciesLogoCollector(session=func_session).collect_symbols_info()
+    else:
+        logger.critical("Database session is not available")
