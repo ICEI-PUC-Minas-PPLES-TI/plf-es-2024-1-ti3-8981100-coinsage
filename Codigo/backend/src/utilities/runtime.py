54c18e1a258f2b4f4cd64fce74aa3c5cb0e2835b
José Victor <jvictormmendesd@gmail.com>
2024-04-01 15:52:47 -0300

    adding method runtime decorator

---
+++ Codigo/backend/src/utilities/runtime.py
@@ -0,0 +1,12 @@
+# Path: src/utilities/runtime.py
+
+import time
+from loguru import logger
+
+def show_runtime(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        logger.info(f"{func.__name__} executed in {time.time() - start_time} seconds")
+        return result
+    return wrapper
