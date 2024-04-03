df8e0676d5651eb37d3b74115e2cfbceac4a09ee
joaopaduameta3 <joao.padua@meta3.com.br>
2024-04-03 09:27:19 -0300

    tabela

--- Codigo/frontend/src/App.tsx
+++ Codigo/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header/Header";
 import { Title } from "./components/Title/Title";
 import { DownloadButton } from './components/DownloadButton/DownloadButton';
+import { Table } from './components/Table/Table';
+
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 import "./styles/global.css";
@@ -9,37 +11,13 @@ import React, { useState } from 'react';
 
 
 export function App() {
-  const [rows, setRows] = useState<any[]>([]);
-
-const columns: GridColDef[] = [
-{ field: 'setor', headerName: 'Setor', width: 150 },
-{ field: 'cripto', headerName: 'Cripto', width: 150 },
-{ field: 'ranking', headerName: 'Ranking', width: 150 },
-{ field: 'valorizacao', headerName: '% Valorização', width: 180 },
-{ field: 'preco', headerName: 'Preço Agora', width: 150 },
-{ field: 'emas', headerName: 'EMAs (d) Alinhados', width: 200 },
-{ field: 'dataValorizacaoVolume', headerName: 'Data Valorização Volume (d)', width: 250 },
-{ field: 'quantidadeVolumeValorizacao', headerName: 'Quantidade Volume Valorização', width: 280 },
-{ field: 'quantidadeVolumeDiaAnterior', headerName: 'Quantidade Volume Dia Anterior', width: 280 },
-{ field: 'percentDiaAnterior', headerName: '% Dia/Dia Anterior', width: 250 },
-{ field: 'ema8', headerName: 'EMA8 (s)', width: 150 },
-];
 
   return (
     <div>
       <Header />
       <Title/>
       <DownloadButton/>
-      <div style={{ marginLeft: '10px', marginTop: '20px' }}>
-        <h2>Estágio 1</h2>
-      </div>
-            <DataGrid
-                columns={columns}
-                rows={rows}
-                rowHeight={38}
-                disableRowSelectionOnClick 
-                autoHeight
-            />
+      <Table/>
     </div>
   );
 }
---
+++ Codigo/frontend/src/components/Table/Table.tsx
@@ -0,0 +1,24 @@
+import styles from './Table.module.css'
+
+export function Table() {
+  return (
+    <table>
+  <tr>
+    <th>Setor</th>
+    <th>Cripto</th>
+    <th>Ranking</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+  );
+}
+
