102ab43af61892e07c8ccc33b06a8a5ffdeddacc
José Victor <jvictormmendesd@gmail.com>
2024-04-04 16:55:01 -0300

    fixing week increase tests

--- Codigo/backend/tests/unit_tests/services/analysis/first_stage/test_week_percentage_val_service.py
+++ Codigo/backend/tests/unit_tests/services/analysis/first_stage/test_week_percentage_val_service.py
@@ -35,16 +35,12 @@ def test_week_percentage_positive_valorization_calculated_correctly(test_session
     today_date = datetime.now()
     last_week_date = today_date - timedelta(days=7)
 
-    fake_first_stage_analysis_last_week = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency.uuid, today=last_week_date, closing_price=1000.0
-    )
-
-    test_session.add(fake_first_stage_analysis_last_week)
-    test_session.commit()
-    test_session.refresh(fake_first_stage_analysis_last_week)
-
     fake_first_stage_analysis_current_week = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency.uuid, today=today_date, closing_price=1100.0
+        uuid_analysis=fake_analysis.uuid,
+        uuid_currency=fake_currency.uuid,
+        today=today_date,
+        closing_price=1100.0,
+        last_week_closing_price=1000,
     )
 
     test_session.add(fake_first_stage_analysis_current_week)
@@ -57,7 +53,7 @@ def test_week_percentage_positive_valorization_calculated_correctly(test_session
     )
 
     symbol = "BTC"
-    result = sut._calculate_week_percentage_valorization(symbol)
+    result = sut._calculate_week_percentage_valorization(symbol, fake_analysis.uuid)
 
     # Assert
     assert result == 10.0
@@ -87,16 +83,12 @@ def test_week_percentage_negative_valorization_calculated_correctly(test_session
     today_date = datetime.now()
     last_week_date = today_date - timedelta(days=7)
 
-    fake_first_stage_analysis_last_week = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency.uuid, today=last_week_date, closing_price=1100.0
-    )
-
-    test_session.add(fake_first_stage_analysis_last_week)
-    test_session.commit()
-    test_session.refresh(fake_first_stage_analysis_last_week)
-
     fake_first_stage_analysis_current_week = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency.uuid, today=today_date, closing_price=1000.0
+        uuid_analysis=fake_analysis.uuid,
+        uuid_currency=fake_currency.uuid,
+        today=today_date,
+        closing_price=1000.0,
+        last_week_closing_price=1100,
     )
 
     test_session.add(fake_first_stage_analysis_current_week)
@@ -109,13 +101,13 @@ def test_week_percentage_negative_valorization_calculated_correctly(test_session
     )
 
     symbol = "BTC"
-    result = sut._calculate_week_percentage_valorization(symbol)
+    result = sut._calculate_week_percentage_valorization(symbol, fake_analysis.uuid)
 
     # Assert
     assert result == pytest.approx(result, -10)
 
 
-def test_calculate_all_week_percentage_valorization(mocker, test_session):
+def test_calculate_all_week_percentage_valorization(test_session):
     # Arrange
     fake_currency1 = CurrencyBaseInfoModel(
         symbol="BTC",
@@ -138,8 +130,22 @@ def test_calculate_all_week_percentage_valorization(mocker, test_session):
         urls=["https://ethereum.com"],
     )
 
-    test_session.add_all([fake_currency1, fake_currency2])
+    fake_currency3 = CurrencyBaseInfoModel(
+        symbol="LTC",
+        cmc_id=3,
+        cmc_slug="litecoin",
+        logo="https://litecoin.com",
+        name="Litecoin",
+        description="The third cryptocurrency",
+        technical_doc=["https://litecoin.com/technical"],
+        urls=["https://litecoin.com"],
+    )
+
+    test_session.add_all([fake_currency1, fake_currency2, fake_currency3])
     test_session.commit()
+    test_session.refresh(fake_currency1)
+    test_session.refresh(fake_currency2)
+    test_session.refresh(fake_currency3)
 
     fake_analysis = Analysis()
     test_session.add(fake_analysis)
@@ -148,54 +154,58 @@ def test_calculate_all_week_percentage_valorization(mocker, test_session):
     today_date = datetime.now()
     last_week_date = today_date - timedelta(days=7)
 
-    fake_first_stage_analysis_last_week_btc = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency1.uuid, today=last_week_date, closing_price=1000.0
-    )
-    fake_first_stage_analysis_last_week_eth = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency2.uuid, today=last_week_date, closing_price=2000.0
-    )
-
-    test_session.add_all([fake_first_stage_analysis_last_week_btc, fake_first_stage_analysis_last_week_eth])
-    test_session.commit()
-
     fake_first_stage_analysis_current_week_btc = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency1.uuid, today=today_date, closing_price=1100.0
+        uuid_analysis=fake_analysis.uuid,
+        uuid_currency=fake_currency1.uuid,
+        today=last_week_date,
+        closing_price=1100.0,
+        last_week_closing_price=1000,
     )
     fake_first_stage_analysis_current_week_eth = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency2.uuid, today=today_date, closing_price=2200.0
+        uuid_analysis=fake_analysis.uuid,
+        uuid_currency=fake_currency2.uuid,
+        today=last_week_date,
+        closing_price=2234.0,
+        last_week_closing_price=2000,
+    )
+    fake_first_stage_analysis_current_week_ltc = FirstStageAnalysisModel(
+        uuid_analysis=fake_analysis.uuid,
+        uuid_currency=fake_currency3.uuid,
+        today=last_week_date,
+        closing_price=3023.23,
+        last_week_closing_price=5842.0,
     )
 
-    test_session.add_all([fake_first_stage_analysis_current_week_btc, fake_first_stage_analysis_current_week_eth])
+    test_session.add_all(
+        [
+            fake_first_stage_analysis_current_week_btc,
+            fake_first_stage_analysis_current_week_eth,
+            fake_first_stage_analysis_current_week_ltc,
+        ]
+    )
     test_session.commit()
-
-    mocker.patch.object(
-        ClosingPriceService,
-        "get_closing_price_by_symbol",
-        return_value=[fake_first_stage_analysis_last_week_btc, fake_first_stage_analysis_current_week_btc],
-    )
+    test_session.refresh(fake_first_stage_analysis_current_week_btc)
+    test_session.refresh(fake_first_stage_analysis_current_week_eth)
+    test_session.refresh(fake_first_stage_analysis_current_week_ltc)
 
     sut = WeekPercentageValorizationService(
         session=test_session, closing_price_service=ClosingPriceService(session=test_session)
     )
 
-    symbols = ["BTC", "ETH"]
+    symbols = ["BTC", "ETH", "LTC"]
 
     # Act
-    result = sut.calculate_all_week_percentage_valorization(symbols)
-    eth_result = [x for x in result if x.uuid_currency == fake_currency2.uuid]
+    result = sut.calculate_all_week_percentage_valorization(symbols, fake_analysis.uuid)
     btc_result = [x for x in result if x.uuid_currency == fake_currency1.uuid]
+    eth_result = [x for x in result if x.uuid_currency == fake_currency2.uuid]
+    ltc_result = [x for x in result if x.uuid_currency == fake_currency3.uuid]
 
     # Assert
     assert result is not None
-    assert len(result) == 4
-    assert btc_result[1].week_increase_percentage == 10.0
-    assert btc_result[0].week_increase_percentage == None
-    assert btc_result[1].today == today_date
-    assert btc_result[0].today == last_week_date
-    assert eth_result[1].week_increase_percentage == 10.0
-    assert eth_result[0].week_increase_percentage == None
-    assert eth_result[1].today == today_date
-    assert eth_result[0].today == last_week_date
+    assert len(result) == 3
+    assert btc_result[0].week_increase_percentage == pytest.approx(10.0, rel=0.01, abs=0.01)
+    assert eth_result[0].week_increase_percentage == pytest.approx(11.7, rel=0.01, abs=0.01)
+    assert ltc_result[0].week_increase_percentage == pytest.approx(-48.2, rel=0.01, abs=0.01)
 
 
 def test_update_last(test_session):
@@ -224,30 +234,26 @@ def test_update_last(test_session):
     last_week_date = today_date - timedelta(days=7)
 
     fake_first_stage_analysis_last_week = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency.uuid, today=last_week_date, closing_price=9764.0
+        uuid_analysis=fake_analysis.uuid,
+        uuid_currency=fake_currency.uuid,
+        today=last_week_date,
+        closing_price=569.52,
+        last_week_closing_price=9764.0,
     )
 
     test_session.add(fake_first_stage_analysis_last_week)
     test_session.commit()
     test_session.refresh(fake_first_stage_analysis_last_week)
 
-    fake_first_stage_analysis_current_week = FirstStageAnalysisModel(
-        uuid_analysis=fake_analysis.uuid, uuid_currency=fake_currency.uuid, today=today_date, closing_price=569.52
-    )
-
-    test_session.add(fake_first_stage_analysis_current_week)
-    test_session.commit()
-    test_session.refresh(fake_first_stage_analysis_current_week)
-
     sut = WeekPercentageValorizationService(
         session=test_session, closing_price_service=ClosingPriceService(session=test_session)
     )
 
     symbol = "BTC"
-    week_percentage = Decimal(-94.17)
+    week_percentage = float(-94.17)
 
     # Act
-    sut.update_last([{symbol: week_percentage}])
+    sut.update_last([{symbol: week_percentage}], fake_analysis.uuid)
 
     # Assert
     updated_model = (
