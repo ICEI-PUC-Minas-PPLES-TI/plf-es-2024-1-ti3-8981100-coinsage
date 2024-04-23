7bbad0451c28203ac924ebce4f9b325a65616a86
José Victor <jvictormmendesd@gmail.com>
2024-04-23 12:10:33 -0300

    styling

--- Codigo/frontend/src/components/Tabela/Tabela.tsx
+++ Codigo/frontend/src/components/Tabela/Tabela.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,57 +7,12 @@ import TableFooter from "@mui/material/TableFooter";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import LastPageIcon from "@mui/icons-material/LastPage";
 import { TableHead } from "@mui/material";
-import api from "../../service/api";
-import { Endpoints } from "../../constants/apiConfig.json";
 import LogoSymbol from "./Logo/LogoSymbol";
 import TextColoredCondition from "./TextColoredCondition/TextColoredCondition";
 import { LinearProgress } from "@mui/material";
-import titleStyles from "../../components/Title/Title.module.css";
-
-const LoadingTableComponent: React.FC = () => {
-  const [loadingText, setLoadingText] = React.useState<string>(
-    "Carregando dados da última análise..."
-  );
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setLoadingText((prev) => {
-        if (prev === "Carregando dados da última análise...") {
-          return "Por favor, aguarde...";
-        } else {
-          return "Carregando dados da última análise...";
-        }
-      });
-    }, 1200);
-
-    return () => clearInterval(interval);
-  });
-
-  return (
-    <>
-      <h1 className={titleStyles.title}>{loadingText}</h1>
-      <Box sx={{ width: "100%" }}>
-        <LinearProgress />
-      </Box>
-    </>
-  );
-};
-
-interface TablePaginationActionsProps {
-  count: number;
-  page: number;
-  rowsPerPage: number;
-  onPageChange: (
-    event: React.MouseEvent<HTMLButtonElement>,
-    newPage: number
-  ) => void;
-}
+import TablePaginationActions from "./TablePaginationActions/TablePaginationActions";
+import tableStyles from "./Tabela.module.css";
 
 interface Column {
   id: string;
@@ -68,76 +21,6 @@ interface Column {
   align?: "right";
   format?: (value: number) => string;
   renderCell?: (params: any) => JSX.Element;
-}
-
-function TablePaginationActions(props: TablePaginationActionsProps) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </Box>
-  );
 }
 
 const areEmasAligned = (value: boolean) =>
@@ -205,45 +88,26 @@ const dataRowsMapper = (data: any) => {
   return newData;
 };
 
-export default function CustomPaginationActionsTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [rows, setRows] = React.useState<any[]>([]);
-  const [initialLoading, setInitialLoading] = React.useState<boolean>(true);
-  const [tableLoading, setTableLoading] = React.useState<boolean>(true);
-  const [count, setCount] = React.useState<number>(0);
-
-  React.useEffect(() => {
-    setTableLoading(true);
-    setRows([]);
-    api
-      .get(
-        `${Endpoints.FirstStage}?limit=${rowsPerPage}&offset=${
-          page * rowsPerPage ?? 0
-        }`
-      )
-      .then((response: any) => {
-        if (response.status >= 200 && response.status < 300) {
-          const data = response.data;
-          if (data) {
-            setRows(dataRowsMapper(data.last_update.data.data));
-            setCount(data.last_update.data.total);
-          } else {
-            console.log("ERROR USUARIO NAO CADASTRADO");
-          }
-        } else {
-          console.log("ERROR AO OBTER");
-        }
-      })
-      .catch(() => {
-        console.log("ULTIMO ERROR AO OBTER");
-      })
-      .finally(() => {
-        setInitialLoading(false);
-        setTableLoading(false);
-      });
-  }, [page, rowsPerPage]);
-
+export default function CustomPaginationActionsTable(
+  {
+    rowsRaw,
+    page,
+    setPage,
+    rowsPerPage,
+    setRowsPerPage,
+    count,
+    tableLoading,
+  }: {
+    rowsRaw: any[];
+    page: number;
+    setPage: React.Dispatch<React.SetStateAction<number>>;
+    rowsPerPage: number;
+    setRowsPerPage: React.Dispatch<React.SetStateAction<number>>;
+    count: number;
+    tableLoading: boolean;
+  }
+) {
+  const rows = dataRowsMapper(rowsRaw);
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length);
 
   const handleChangePage = (
@@ -259,14 +123,6 @@ export default function CustomPaginationActionsTable() {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-
-  if (initialLoading) {
-    return (
-      <>
-        <LoadingTableComponent />
-      </>
-    );
-  }
 
   return (
     <TableContainer component={Paper}>
@@ -285,8 +141,8 @@ export default function CustomPaginationActionsTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id} style={{ height: 40}}>
+          {rows.map((row: any) => (
+            <TableRow key={row.id} style={{ height: 40}} className={tableStyles.oddRowStyle}>
               <TableCell component="th" scope="row">
                 {row.setor}
               </TableCell>
@@ -324,7 +180,7 @@ export default function CustomPaginationActionsTable() {
           <TableRow>
             <TablePagination
               rowsPerPageOptions={[10, 20, 50, 100]}
-              colSpan={10}
+              colSpan={columns.length}
               count={count}
               rowsPerPage={rowsPerPage}
               page={page}
--- Codigo/frontend/src/pages/Balanceamento/Balanceamento.tsx
+++ Codigo/frontend/src/pages/Balanceamento/Balanceamento.tsx
@@ -1,86 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { Fade, Zoom } from '@mui/material';
-import titleStyles from '../../components/Title/Title.module.css'
-import Title from '../../components/Title/Title'
-// import  DownloadButton  from '../../components/DownloadButton'
-import CustomPaginationActionsTable from '../../components/Tabela/Tabela'
-import styles from './Balanceamento.module.css'
-import { Endpoints } from '../../constants/apiConfig.json'
-import api from '../../service/api'
-import { Box, LinearProgress } from '@mui/material'
+import React, { useEffect, useState } from "react";
+import { Fade, Zoom } from "@mui/material";
+import Title from "../../components/Title/Title";
+import CustomPaginationActionsTable from "../../components/Tabela/Tabela";
+import styles from "./Balanceamento.module.css";
+import { Endpoints } from "../../constants/apiConfig.json";
+import api from "../../service/api";
+import { Box, LinearProgress } from "@mui/material";
+import LoadingTableComponent from "../../components/Tabela/LoadingTableComponent/LoadingTableComponent";
 
-const LoadingTableComponent: React.FC = () => {
-  const [loadingText, setLoadingText] = useState<string>('Carregando dados da última análise...');
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setLoadingText((prev) => {
-        if (prev === 'Carregando dados da última análise...') {
-          return 'Por favor, aguarde...';
-        } else {
-          return 'Carregando dados da última análise...';
-        }
-      });
-    }, 1200);
-
-    return () => clearInterval(interval);
-  })
-
-
-  return (
-    <>
-      <h1 className={titleStyles.title}>{loadingText}</h1>
-      <Box sx={{ width: '100%' }}>
-        <LinearProgress />
-      </Box>
-    </>
-  );
-}
 
 const Balanceamento: React.FC = () => {
-  const [lastUpdate, setLastUpdate] = useState<string>('');
-  const [tableData, setTableData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState<any[]>([]);
+  const [initialLoading, setInitialLoading] = useState<boolean>(true);
+  const [tableLoading, setTableLoading] = useState<boolean>(true);
+  const [count, setCount] = useState<number>(0);
+  const [lastUpdate, setLastUpdate] = useState<string>("");
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   api
-  //     .get(Endpoints.FirstStage)
-  //     .then((response: any) => {
-  //       if (response.status >= 200 && response.status < 300) {
-  //         const data = response.data;
-  //         if (data) {
-  //           setLastUpdate(data.last_update.time);
-  //           setTableData(data.last_update.data.data);
-  //         } else {
-  //           console.log('ERROR USUARIO NAO CADASTRADO');
-  //         }
-  //       } else {
-  //         console.log('ERROR AO OBTER');
-  //       }
-  //     })
-  //     .catch(() => {
-  //       console.log('ULTIMO ERROR AO OBTER');
-  //     })
-  //     .finally(() => {
-  //       setLoading(false);
-  //     })
-  // }, []);
+  useEffect(() => {
+    setTableLoading(true);
+    setRows([]);
+    api
+      .get(
+        `${Endpoints.FirstStage}?limit=${rowsPerPage}&offset=${
+          page * rowsPerPage ?? 0
+        }`
+      )
+      .then((response: any) => {
+        if (response.status >= 200 && response.status < 300) {
+          const data = response.data;
+          if (data) {
+            setRows(data.last_update.data.data);
+            setCount(data.last_update.data.total);
+            setLastUpdate(data.last_update.time);
+          } else {
+            console.log("ERROR USUARIO NAO CADASTRADO");
+          }
+        } else {
+          console.log("ERROR AO OBTER");
+        }
+      })
+      .catch(() => {
+        console.log("ULTIMO ERROR AO OBTER");
+      })
+      .finally(() => {
+        setInitialLoading(false);
+        setTableLoading(false);
+      });
+  }, [page, rowsPerPage]);
 
   return (
     <div className={styles.content}>
-      {/* {loading ?
+      {initialLoading ? (
         <>
           <LoadingTableComponent />
         </>
-        :
+      ) : (
         <>
           <Title lastUpdate={lastUpdate} />
+          <CustomPaginationActionsTable
+            rowsRaw={rows}
+            page={page}
+            setPage={setPage}
+            rowsPerPage={rowsPerPage}
+            setRowsPerPage={setRowsPerPage}
+            count={count}
+            tableLoading={tableLoading}
+          />
         </>
-      } */}
-      <CustomPaginationActionsTable />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Balanceamento;
