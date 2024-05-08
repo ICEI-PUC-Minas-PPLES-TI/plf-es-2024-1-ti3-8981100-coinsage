c7f0186b8096545121d8b49009b072362287a0ee
José Victor <jvictormmendesd@gmail.com>
2024-05-08 14:42:01 -0300

    fixing api call

--- Codigo/frontend/src/components/AnaliseHeader/DownloadButton/DownloadButton.tsx
+++ Codigo/frontend/src/components/AnaliseHeader/DownloadButton/DownloadButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import DownloadForOfflineRoundedIcon from '@mui/icons-material/DownloadForOfflineRounded';
 import { Endpoints } from "../../../constants/apiConfig.json";
-import axios from 'axios'; 
+import api from "../../../service/api";
 
 import styles from './DownloadButton.module.css'
 
@@ -13,17 +13,17 @@ interface DownloadPartProps {
 const DownloadButton: React.FC<DownloadPartProps> = ({ title, loading }) => {
     const handleDownload = async () => {
         try {
-            const response = await axios.get(Endpoints.Workbook, {
+            const response = await api.get(Endpoints.Workbook, {
                 responseType: 'blob',
             });
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            
+
             const date = new Date();
             const formattedDate = `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear().toString().slice(-2)}`;
             const filename = `análise ${formattedDate}.xlsx`;
-    
+
             link.setAttribute('download', filename);
             document.body.appendChild(link);
             link.click();
