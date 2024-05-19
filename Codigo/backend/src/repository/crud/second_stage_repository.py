527cfdf0b4595aa0c8e5444cc926c6a15ffa29e6
=Lt Ellen Ripley <try@hackme.com>
2024-05-19 19:06:20 -0300

    feature: Variation Price for second stage of analisys

---
+++ Codigo/backend/src/repository/crud/second_stage_repository.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from loguru import logger
+from sqlalchemy import select, Uuid
+from sqlalchemy.orm import Session
+
+from src.models.db.currency_base_info import CurrencyBaseInfoModel
+from src.models.db.second_stage_analysis import SecondStageAnalysisModel
+from src.models.schemas.analysis.first_stage_analysis import VolumeAnalysis
+
+
+def add_variation_analysis(
+    db: Session, variation_analysis_data: List[VolumeAnalysis], analysis_indentifier: Uuid
+) -> None:
+    try:
+        for data in variation_analysis_data:
+            currency_info = db.execute(
+                select(CurrencyBaseInfoModel).where(CurrencyBaseInfoModel.symbol == data["symbol"])
+            ).scalar()
+
+            if currency_info:
+                new_analysis = SecondStageAnalysisModel(
+                    uuid_analysis=analysis_indentifier,
+                    uuid_currency=currency_info.uuid,
+                    year_variation_per=data["year_variation_per"],
+                    semester_variation_per=data["semester_variation_per"],
+                    quarter_variation_per=data["quarter_variation_per"],
+                    month_variation_per=data["month_variation_per"],
+                    week_variation_per=data["week_variation_per"],
+                )
+
+                db.commit()
+                db.add(new_analysis)
+            else:
+                logger.info(f"Moeda com símbolo {data['symbol']} não encontrada.")
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        logger.info(f"Erro ao adicionar análises: {e}")
+    finally:
+        db.close()
---
+++ Codigo/backend/src/services/analysis/second_stage/variation_per_service.py
@@ -0,0 +1,81 @@
+import concurrent.futures
+import logging
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+from binance.spot import Spot
+from sqlalchemy import Uuid
+from sqlalchemy.orm import Session
+
+from src.models.schemas.analysis.second_stage_analysis import VariationPerSchema
+from src.repository.crud import second_stage_repository
+from src.services.externals.binance_price_at_timestamp import BinancePriceAtTimestampService
+from src.services.externals.binance_symbol_colletor import BinanceSymbolCollector
+from src.utilities.runtime import show_runtime
+
+logger = logging.getLogger(__name__)
+
+
+class VariationPer:
+    def __init__(self, session: Session):
+        self.session = session
+        self.repository = second_stage_repository
+        self.binance_symbol_collector = BinanceSymbolCollector()
+        self.binance_price_at_timestamp = BinancePriceAtTimestampService()
+
+    def collect_variation_price(self) -> list[VariationPerSchema]:
+        all_symbols = self.binance_symbol_collector.get_symbols()
+        dates_to_collect = self.dates_to_collect()
+        variation_per_list = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+            future_to_symbol = {
+                executor.submit(self.process_symbol, symbol, self.dates_to_collect): symbol for symbol in all_symbols
+            }
+            for future in concurrent.futures.as_completed(future_to_symbol):
+                variation_per_list.append(future.result())
+
+        return variation_per_list
+
+    def process_symbol(self, symbol, dates_to_collect):
+        symbol_data = {"symbol": symbol.base_asset}
+        for date_info in dates_to_collect:
+            date_key, days_back = list(date_info.items())[0]
+            price = self.binance_price_at_timestamp.get_by_symbol(symbol.base_asset, days_back)
+            symbol_data[date_key] = price
+
+        current_price = Spot().ticker_price(symbol.symbol)
+        symbol_data["current_price"] = Decimal(current_price["price"]) if current_price else None
+        return self.extract_variation(symbol_data)
+
+    def dates_to_collect(self) -> list[dict]:
+
+        days = {
+            "week_variation_per": 7,
+            "month_variation_per": 30,
+            "quarter_variation_per": 90,
+            "semester_variation_per": 180,
+            "year_variation_per": 360,
+        }
+        datetime_now = datetime.now()
+        dates_to_collect = []
+
+        for variation_per, day in days.items():
+            dates_to_collect.append({variation_per: datetime_now - timedelta(day)})
+        return dates_to_collect
+
+    def extract_variation(self, symbol_data) -> list[VariationPerSchema]:
+        current_price = symbol_data["current_price"]
+        del symbol_data["current_price"]
+        for key, value in symbol_data.items():
+            if value and isinstance(value, Decimal) and current_price:
+                symbol_data[key] = current_price - value
+
+        return symbol_data
+
+    @show_runtime
+    def fetch_variation_price(self, analysis_identifier: Uuid) -> None:
+        variation_price = self.collect_variation_price()
+        self.repository.add_variation_analysis(
+            db=self.session, variation_analysis_data=variation_price, analysis_indentifier=analysis_identifier
+        )
