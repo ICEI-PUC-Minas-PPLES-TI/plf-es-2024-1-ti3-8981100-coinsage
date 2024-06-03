10464c90fbbd124e433019ba269798d578e08d3d
José Victor <jvictormmendesd@gmail.com>
2024-06-03 13:54:57 -0300

    adding active flag to coins

--- Codigo/backend/src/repository/crud/currency_info_repository.py
+++ Codigo/backend/src/repository/crud/currency_info_repository.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from loguru import logger
 from sqlalchemy import or_, select, Uuid
 from sqlalchemy.orm import Session
 
@@ -39,7 +40,11 @@ def get_currency_info_by_uuid(db: Session, uuid_value: Uuid) -> currency_base_in
 
 
 def get_cryptos(db: Session, skip: int = 0, limit: int = 10000) -> list[currency_base_info.CurrencyBaseInfoModel]:
-    return db.query(currency_base_info.CurrencyBaseInfoModel).all()
+    return (
+        db.query(currency_base_info.CurrencyBaseInfoModel)
+        .filter(currency_base_info.CurrencyBaseInfoModel.active == True)
+        .all()
+    )
 
 
 def get_currency_info_by_symbol(db: Session, symbol: str) -> currency_base_info.CurrencyBaseInfoModel | None:
@@ -80,3 +85,18 @@ def get_coins_by_sector(db: Session, sector_uuid: Uuid) -> List[currency_base_in
         .filter(SetorCurrencyBaseInfo.uuid_setor == sector_uuid)
         .all()
     )
+
+
+def clear_inactive(db: Session, cmc_symbols: List[str]) -> None:
+    symbols = (
+        db.query(currency_base_info.CurrencyBaseInfoModel)
+        .filter(currency_base_info.CurrencyBaseInfoModel.symbol.notin_(cmc_symbols))
+        .all()
+    )
+
+    logger.warning(f"Clearing {len(symbols)} inactive symbols {[symbol.symbol for symbol in symbols]}")
+
+    for symbol in symbols:
+        symbol.active = False
+
+    db.commit()
